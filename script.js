6e15f98e8749b40c3e3c2e04cc23e92a90774346
7ASL <202996150+7ASL@users.noreply.github.com>
2026-04-02 01:46:19 +0200

    Add create grid functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+const cellTemplate = document.querySelector("#cell-template");
+const container = document.querySelector("#container");
+
+function createGrid(size) {
+    container.replaceChildren();
+    
+    
+    for(let i = 0; i < size; i++){
+        let row = document.createElement("div");
+        row.id = "row";
+        for(let j = 0; j < size; j++){
+            let cell = cellTemplate.content.cloneNode(true);
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }        
+}
+
+createGrid(16);
